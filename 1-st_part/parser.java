9f39cb5f3d933aa339d0104d88c026ffd75cee8c
Dimitris Dimochronis <dimitris.dimochronis@gmail.com>
2025-03-26 13:55:44 +0200

    Syntax check for chars, exit w/ q and parse number

--- 1-st_part/parser.java
+++ 1-st_part/parser.java
@@ -3,10 +3,51 @@ import java.io.*;
 public class parser {
     private static String input;
 
+    private static int index = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while ((input = reader.readLine()) != null) {
-            System.out.println(input);
+            
+            // If input is "q" then exit
+            if (input.equals("q")) {
+                System.out.println("Exit");
+                break;
+            }
+
+            // First syntax error check
+            if (input.length() == 0 || input.length() == 2) {
+                System.out.println("Syntax error: length");
+                continue;
+            }
+
+            // Error if something differs than '+', '-', '**' and numbers or whitespaces
+            if (!input.matches("[0-9\\+\\-\\*\\s]+")) {
+                System.out.println("Syntax error: invalid characters");
+                continue;
+            }
+
+            // Remove all spaces from input
+            //input = input.replaceAll("\\s", "");
+
+            // Parse number
+            //int number = parseNumber();
+            //System.out.println("Number: " + number);
+
+            System.out.println("Input: " + input);
         }
     }
+
+    // Parse number from input
+    private static int parseNumber() {
+        
+        int number = 0;
+        while (index < input.length() && Character.isDigit(input.charAt(index))) {
+            number = number * 10 + (input.charAt(index) - '0');
+            index++;
+        }
+
+        return number;
+    }
+
 }
