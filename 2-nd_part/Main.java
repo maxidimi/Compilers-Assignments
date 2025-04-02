e873f5dcb1c3cdbf48583e2985109bfc91c6d664
Dimitris Dimochronis <dimitris.dimochronis@gmail.com>
2025-04-02 15:01:17 +0300

    Parse single def and single call and put them java

--- 2-nd_part/Main.java
+++ 2-nd_part/Main.java
@@ -3,7 +3,14 @@ import java.io.*;
 
 class Main {
     public static void main(String[] argv) throws Exception{
+        // Redirect stdout and stderr to Translated.java
+        PrintStream out = new PrintStream(new FileOutputStream("Translated.java"));
+        System.setOut(out);
+        System.setErr(out);
+
         Parser p = new Parser(new Scanner(new InputStreamReader(System.in)));
         p.parse();
+        
+        out.close();
     }
 }
