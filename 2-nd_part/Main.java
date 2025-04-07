c51a99a14e066f97ee64adef8556a6256e32ff87
Dimitris Dimochronis <dimitris.dimochronis@gmail.com>
2025-04-07 20:39:14 +0300

    Handle suffix in IR

--- 2-nd_part/Main.java
+++ 2-nd_part/Main.java
@@ -3,13 +3,23 @@ import java.io.*;
 
 class Main {
     public static void main(String[] argv) throws Exception{
-        // Redirect stdout and stderr to Translated.java
-        PrintStream out = new PrintStream(new FileOutputStream("Translated.java"));
-        System.setOut(out);
+        // Redirect IR parser output to Translated.ir
+        PrintStream out1 = new PrintStream(new FileOutputStream("Translated.ir"));
+        System.setOut(out1);
 
-        Parser p = new Parser(new Scanner(new InputStreamReader(System.in)));
+        // Parse first through Parser1 for IR
+        Parser1 p1 = new Parser1(new Scanner(new InputStreamReader(System.in)));
+        p1.parse();
+        out1.close();
+
+        // Redirect output to Translated.java
+        PrintStream out2 = new PrintStream(new FileOutputStream("Translated.java"));
+        System.setOut(out2);
+
+        // Read the generated IR file and parse it to java code
+        Parser p = new Parser(new Scanner(new FileReader("Translated.ir")));
         p.parse();
         
-        out.close();
+        out2.close();
     }
 }
